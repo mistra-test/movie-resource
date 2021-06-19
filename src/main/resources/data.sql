6779645481142214c60ae60b056e62d24e445758
Mistra <sperectrum@gmail.com>
2021-06-19 16:28:53 +0200

    change native version

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,3 @@
-CREATE TABLE IF NOT EXISTS movie (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL
-);
-
 INSERT INTO movie (id, name, description) VALUES
   (1, 'Lord of the rings', 'Frodo and Sam are on holiday'),
   (2, 'Iron Man', 'W Tony Stark'),
